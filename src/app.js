eaa804677b4da6bbf90cfdf3b11e4b38f73b09d4
Lucas Benchinol <lucasbenchinol@gmail.com>
2025-11-03 16:49:06 -0300

    .

--- src/app.js
+++ src/app.js
@@ -26,4 +26,4 @@ app.use('/api/adoptions', adoptionsRouter);
 app.use('/api/sessions', sessionsRouter);
 app.use('/api/mocks', mocksRouter);
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
